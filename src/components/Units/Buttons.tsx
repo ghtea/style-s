c8d97a67be1d2a994628bcfa8dc87e5a593f3d98
Jaehyeon Park <47841931+ghtea@users.noreply.github.com>
2020-10-14 20:27:33 +0900

    sass, typescript, css module, classname work together now

--- src/components/Units/Buttons.tsx
+++ src/components/Units/Buttons.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import styles from "./Buttons.scss"; // .css not work
+
+import classNames from "classnames/bind"; // easy to forget bind!
+import styles from "./Buttons.module.scss"; 
 // css module not works yet
 
 type PropsButtons = {};
 
+const cx = classNames.bind(styles);
+
 function Buttons({}: PropsButtons) {
   return (
-    <div className="container-button">
-      <button className={styles.button__1}>button1</button>
+    <div className={cx('collection_button')}>
+
+      <div>
+        <button className={cx('button_1')}> button 1 </button>
+      </div>
+     
+      <div>
+        <button className={cx('button_2')}> button 2 </button>
+      </div>
+
     </div>
   );
 }
