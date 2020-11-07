87aba92089342cd251dfaccb18dd84fd2719563a
Jaehyeon Park <47841931+ghtea@users.noreply.github.com>
2020-11-07 23:13:10 +0900

    remaknig css sidebar

--- src/components/Works/Nav/WorkNav1.tsx
+++ src/components/Works/Nav/WorkNav1.tsx
@@ -8,26 +8,29 @@ type PropsWorkNav1 = {};
 
 const cn_root = classNames.bind(styles_root);
 
+// focused on studying gpower of css not js
 function WorkNav1({}: PropsWorkNav1) {
   return (
     <div className={cn_root("body")}>
-
+      
       <div className={cn_root("sidebar")}>
         <div>Home</div>
         <div>Products</div>
         <div>Details</div>
-        <div>Where to buy</div>
-        <div>Contact</div>
-        <div>About</div>
+        <div>Details</div>
       </div>
 
-      <input type="checkbox" className={cn_root("sidebar-btn")} checked/>
-      <label for="sidebar-btn"></label>
+      <input
+        type="checkbox"
+        id="button__toggle-sidebar"
+        className={cn_root("button__toggle-sidebar")}
+      />
+      <label htmlFor="button__toggle-sidebar"></label>
 
       <div className={cn_root("content")}>
         <h1>Flat CSS Sidebar</h1>
+        <div>ddddddddddddddddddddddd</div>
       </div>
-
     </div>
   );
 }
