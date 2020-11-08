f3f42fa085dae05c9a2ec7dfe1d28b6f8157d064
Jaehyeon Park <47841931+ghtea@users.noreply.github.com>
2020-11-08 14:06:52 +0900

    added tooltip with triangle study

--- src/components/Works/Action/WorkAction1.tsx
+++ src/components/Works/Action/WorkAction1.tsx
@@ -11,7 +11,18 @@ const cn_root = classNames.bind(styles_root);
 function WorkAction1({}: PropsWorkAction1) {
   return (
     <div className={cn_root("root")}>
-      action1
+      <div className={cn_root("object", "top")} data-tooltip="I’m the tooltip text.">
+        top
+      </div>
+      <div className={cn_root("object", "right")} data-tooltip="I’m the tooltip text.">
+        right
+      </div>
+      <div className={cn_root("object", "bottom")} data-tooltip="I’m the tooltip text.">
+        bottom
+      </div>
+      <div className={cn_root("object", "left")} data-tooltip="I’m the tooltip text.">
+        left
+      </div>
     </div>
   );
 }
